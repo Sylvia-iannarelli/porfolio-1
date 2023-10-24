91d1a2ba4e203b0c83d9a387b2ed1eaea9a49844
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2023-10-24 16:50:21 +0200

    Suppr logos

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+import LogoSylvia from 
+
+const Home = () => {
+  return (
+    <div className='container home-page'>
+      <div className='text-zone'>
+        <h1>Hi, <br /> I'm</h1>
+        <img src={} alt="dev" />
+      </div>
+    </div>
+  )
+};
+
+export default Home;
