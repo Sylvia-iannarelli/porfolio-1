47f2f2b9662d673af3f717cc16cf3e39e3f56f5b
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2023-10-30 17:55:14 +0100

    Portfolio avec fausses données, à compléter

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,40 @@
 import './styles.scss';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import portfolioData from '../../data/portfolio.json';
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {
+          portfolio.map((port, idx) => {
+            return (
+              <div className="image-box" key={idx}>
+                <img
+                  src={port.cover}
+                  className="portfolio-image"
+                  alt="couverture du projet"
+                />
+                <div className="content">
+                  <p className="title">{port.title}</p>
+                  <h4 className="description">{port.description}</h4>
+                  <button
+                    type="button"
+                    className="btn"
+                    onClick={() => window.open(port.url)}
+                  >Voir le projet
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -22,7 +53,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        {/* <div>{renderPortfolio()}</div> */}
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
     </>
   );
